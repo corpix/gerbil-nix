658704461365bc886d71afab67e2f1f7f85203cc
Dmitry Moskowski <me@corpix.ru>
2024-07-14 20:13:36 +0000

    update gerbil to ba78b313e82c064fc1c153a93df2673e41c6d4f3

--- gerbil.nix
+++ gerbil.nix
@@ -7,11 +7,11 @@ callPackage ./gerbil-builder.nix rec {
   src = fetchFromGitHub {
     owner = "mighty-gerbils";
     repo = "gerbil";
-    rev = "708c85bda352e9aa2413e8690f61b8a51cc6ade1";
-    hash = "sha256-e1Lkudu8XTLyZl//othmY5D18nJxILaIJNaVOwv5MVc=";
+    rev = "ba78b313e82c064fc1c153a93df2673e41c6d4f3";
+    hash = "sha256-ohoS4kOsMa/lVe+9iy0M+0mQfEA3t6nzFCL8IeXo3Yk=";
     fetchSubmodules = true;
   };
-  gerbil-git-version = "0.18.1-111-g708c85bd";
+  gerbil-git-version = "0.18.1-125-gba78b313";
 
   inherit gambit-git zlib openssl sqlite enableShared;
 }
